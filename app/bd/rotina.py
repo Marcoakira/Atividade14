b9dd40a2706d02c0e1c78eccc28b055befff8479
Dr Marco Aurélio Menezes <tetissu@gmail.com>
2022-04-22 23:39:55 -0300

    fim

--- app/bd/rotina.py
+++ app/bd/rotina.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from time import sleep
 from app.connector_and_menu.postgress import Conector_postgres
 conector = Conector_postgres()
 
@@ -34,6 +35,22 @@ def cad_vendas():
     Função que cadastra as vendas
     """
     vendas_files = pd.read_csv("vendas.csv")
+    menor_valor = 0
+    maior_valor = 10
+    new_table = vendas_files['id_venda'] > menor_valor and vendas_files['id_venda'] < maior_valor
+    loop = 0
+    quantidade = int(input("quantos testes deseja realizar?"))
+    while loop < quantidade:
+        for index, row in new_table.iterrows():
+            conector.inserir(f'''insert into Vendas ( id_funcionario, id_produto, id_cliente, data_venda, valor_venda) values ( {row['id_funcionario']}, {row['id_produto']}, {row['id_cliente']}, '{row['data_venda']}', {row['valor_venda']});''')
+            loop += 1
+            menor_valor += 10
+            maior_valor += 10
+            print(f"{loop} testes realizados, total de {maior_valor} dados inseridos, aguarde 1 minuto até o proximo teste...")
+            if loop == quantidade:
+                print(f"finalizamos os testes")
+                break
+            sleep(60)
 
     for index, row in vendas_files.iterrows():
         conector.inserir(f'''insert into Vendas ( id_funcionario, id_produto, id_cliente, data_venda, valor_venda) values ( {row['id_funcionario']}, {row['id_produto']}, {row['id_cliente']}, '{row['data_venda']}', {row['valor_venda']});''')
